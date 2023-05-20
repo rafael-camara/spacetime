a3361aaee0c73e82f86c389ca9503855def1884d
Rafael Câmara <rafaelcamara.377@gmail.com>
2023-05-19 23:14:33 -0300

    Revert "auth routes with JWT"
    This reverts commit 7262c487892d89abf0e79869f5919f8b3aee811f.

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -3,15 +3,8 @@ import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
-  app.addHook('preHandler', async (request) => {
-    await request.jwtVerify()
-  })
-
-  app.get('/memories', async (request) => {
+  app.get('/memories', async () => {
     const memories = await prisma.memory.findMany({
-      where: {
-        userId: request.user.sub,
-      },
       orderBy: {
         createdAt: 'asc',
       },
@@ -26,7 +19,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
     })
   })
 
-  app.get('/memories/:id', async (request, reply) => {
+  app.get('/memories/:id', async (request) => {
     const paramsSchema = z.object({
       id: z.string().uuid(),
     })
@@ -38,10 +31,6 @@ export async function memoriesRoutes(app: FastifyInstance) {
         id,
       },
     })
-
-    if (!memory.isPublic && memory.userId !== request.user.sub) {
-      return reply.status(401).send()
-    }
 
     return memory
   })
@@ -60,14 +49,14 @@ export async function memoriesRoutes(app: FastifyInstance) {
         content,
         coverUrl,
         isPublic,
-        userId: request.user.sub,
+        userId: '94b7250e-00bf-4751-a070-4f42e6f586d5',
       },
     })
 
     return memory
   })
 
-  app.put('/memories/:id', async (request, reply) => {
+  app.put('/memories/:id', async (request) => {
     const paramsSchema = z.object({
       id: z.string().uuid(),
     })
@@ -82,17 +71,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
 
     const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
 
-    let memory = await prisma.memory.findUniqueOrThrow({
-      where: {
-        id,
-      },
-    })
-
-    if (memory.userId !== request.user.sub) {
-      return reply.status(401).send()
-    }
-
-    memory = await prisma.memory.update({
+    const memory = await prisma.memory.update({
       where: {
         id,
       },
@@ -106,22 +85,12 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.delete('/memories/:id', async (request, reply) => {
+  app.delete('/memories/:id', async (request) => {
     const paramsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = paramsSchema.parse(request.params)
-
-    const memory = await prisma.memory.findUniqueOrThrow({
-      where: {
-        id,
-      },
-    })
-
-    if (memory.userId !== request.user.sub) {
-      return reply.status(401).send()
-    }
 
     await prisma.memory.delete({
       where: {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,6 @@
-import 'dotenv/config'
-
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
-import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -12,11 +8,6 @@ app.register(cors, {
   origin: true,
 })
 
-app.register(jwt, {
-  secret: 'spacetime',
-})
-
-app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
